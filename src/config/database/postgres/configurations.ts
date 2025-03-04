34a1ad06f229632881f1225d116c68891ebb4f90
dvktr <victormendes04012004@gmail.com>
2025-03-04 14:51:27 -0300

    (GUIA): adjust migrations path

--- src/config/database/postgres/configurations.ts
+++ src/config/database/postgres/configurations.ts
@@ -3,13 +3,13 @@ import 'dotenv/config';
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: Number(process.env.DATABASE_PORT) || 5432,
-  username: process.env.DATABASE_USER || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'postgres',
-  database: process.env.DATABASE_NAME || 'guia_db',
-  entities: [`${__dirname}/**/*.entity{.ts,.js}`],
-  migrations: ['dist/database/migrations/*{.ts,.js}'],
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [`dist/**/*.entity{.ts,.js}`],
+  migrations: [`dist/database/migration/{.ts,*.js}`],
   migrationsRun: true,
   logging: true,
 });
