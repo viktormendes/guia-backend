3652cc7566bf39478be22281a147215cee0bb85b
dvktr <victormendes04012004@gmail.com>
2025-03-04 14:51:12 -0300

    (GUIA): import educator and discipline module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostgresConfigModule } from './config/database/postgres/config.module';
+import { DisciplineModule } from './models/discipline/discipline.module';
+import { EducatorModule } from './models/educator/educator.module';
+import { TimetableModule } from './models/timetable/timetable.module';
+import { PrerequisiteModule } from './models/prerequisite/prerequisite.module';
 
 @Module({
-  imports: [PostgresConfigModule],
+  imports: [
+    PostgresConfigModule,
+    DisciplineModule,
+    EducatorModule,
+    TimetableModule,
+    PrerequisiteModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
