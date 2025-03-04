c25b9f39a4c3643fd90208b8ad430ad4784c67fc
dvktr <victormendes04012004@gmail.com>
2025-03-04 14:50:56 -0300

    (GUIA): use class validator and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig } from './config/swagger/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      disableErrorMessages: false,
+    }),
+  );
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.SERVER_PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
