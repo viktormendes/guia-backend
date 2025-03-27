cb0ad56be2929e5e5de794540dc6f3f4c9f9267e
dvktr <victormendes04012004@gmail.com>
2025-03-27 12:08:28 -0300

    (GUIA): import room module

--- src/models/timetable/timetable.module.ts
+++ src/models/timetable/timetable.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Educator } from '../educator/entities/educator.entity';
 import { Discipline } from '../discipline/entities/discipline.entity';
 import { Timetable } from './entities/timetable.entity';
+import { Room } from '../room/entities/room.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Discipline, Educator, Timetable])],
+  imports: [TypeOrmModule.forFeature([Discipline, Educator, Timetable, Room])],
   controllers: [TimetableController],
   providers: [TimetableService],
 })
