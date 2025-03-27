30aac1bba8f3875eb5cefadc393386a7cda9a870
dvktr <victormendes04012004@gmail.com>
2025-03-27 12:58:36 -0300

    (GUIA): adjust migration order

--- src/database/migration/1743091057942-add_room_in_timetable.ts
+++ src/database/migration/1743091057942-add_room_in_timetable.ts
@@ -2,18 +2,18 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddRoomToTimetable1741020387278 implements MigrationInterface {
+export class AddRoomInTimetable1743091057942 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(`
-      ALTER TABLE timetable
-      ADD COLUMN room_id INT REFERENCES room(id) ON DELETE SET NULL;
-    `);
+          ALTER TABLE timetable
+          ADD COLUMN room_id INT REFERENCES room(id) ON DELETE SET NULL;
+        `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(`
-      ALTER TABLE timetable
-      DROP COLUMN room_id;
-    `);
+          ALTER TABLE timetable
+          DROP COLUMN room_id;
+        `);
   }
 }
