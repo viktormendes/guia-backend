8949f6c3791919c7512456d818e5d026b76086ff
dvktr <victormendes04012004@gmail.com>
2025-03-27 12:07:28 -0300

    (GUIA): migration to create table room

---
+++ src/database/migration/1743085383017-create_table_room.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTableRoom1743085383017 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+            CREATE TABLE room (
+            id serial  NOT NULL,
+            description varchar(255)  NOT NULL,
+            floor int  NOT NULL,
+            capacity int  NOT NULL,
+            block_id int  NOT NULL,
+            CONSTRAINT room_pk PRIMARY KEY (id)
+            );`);
+  }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`DROP TABLE room`);
+  }
+}
