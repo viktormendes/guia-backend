924a1905a8ab23cf59eb44430106e414a9a64567
dvktr <victormendes04012004@gmail.com>
2025-03-27 12:07:41 -0300

    (GUIA): migration to create table block

---
+++ src/database/migration/1743085366600-create_table_block.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTableBlock1743085366600 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+            CREATE TABLE block (
+            id serial  NOT NULL,
+            description varchar(255)  NOT NULL,
+            number_of_floors int  NOT NULL,
+            status varchar(255)  NOT NULL,
+            CONSTRAINT block_pk PRIMARY KEY (id)
+            );
+            `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`DROP TABLE block`);
+  }
+}
