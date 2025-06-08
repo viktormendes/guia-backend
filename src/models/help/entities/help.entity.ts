8374721e263fe73fefa8107c1f8e7d6653c64c81
dvktr <victormendes04012004@gmail.com>
2025-06-08 20:08:51 -0300

    dev(GUIA-012): create help base entity

---
+++ src/models/help/entities/help.entity.ts
@@ -0,0 +1,62 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../../../models/user/entities/user.entity';
+import { HelpStatus } from '../enums/help-status.enum';
+import { HelpType } from '../enums/help-type.enum';
+
+@Entity('help')
+export class Help {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'enum',
+    enum: HelpType,
+    default: HelpType.CHAT,
+  })
+  type: HelpType;
+
+  @Column({
+    type: 'enum',
+    enum: HelpStatus,
+    default: HelpStatus.PENDING,
+  })
+  status: HelpStatus;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'student_id' })
+  student: User;
+
+  @ManyToOne(() => User, { nullable: true })
+  @JoinColumn({ name: 'helper_id' })
+  helper: User;
+
+  @Column('jsonb', { nullable: true })
+  log: {
+    action: string;
+    timestamp: Date;
+    details?: any;
+  }[];
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @Column({ name: 'started_at', nullable: true })
+  startedAt: Date;
+
+  @Column({ name: 'completed_at', nullable: true })
+  completedAt: Date;
+
+  @Column({ name: 'cancelled_at', nullable: true })
+  cancelledAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
