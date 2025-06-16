03f30c85612889ec6b3c4c725d279ff5c16b0574
dvktr <victormendes04012004@gmail.com>
2025-06-16 19:44:22 -0300

    dev(GUIA-013): add id to receive local requests

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   });
   SwaggerModuleConfig.setup(app);
 
-  await app.listen(process.env.SERVER_PORT ?? 3000);
+  await app.listen(process.env.SERVER_PORT ?? 3000, '0.0.0.0');
 }
 
 void bootstrap();
